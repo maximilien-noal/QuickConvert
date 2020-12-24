f0de1e87405e091de17a7a14f40369f3212ebe40
Maximilien Noal <noal.maximilien@gmail.com>
2020-12-24 15:56:32 +0100

    Don't display 'done' unless we are converting
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ Converters/BooleanToInvertVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace QuickConvert.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class BooleanToInvertVisibilityConverter : IValueConverter
+    {
+        public static readonly BooleanToInvertVisibilityConverter Default = new BooleanToInvertVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolean)
+            {
+                if (boolean == false)
+                {
+                    return Visibility.Collapsed;
+                }
+                return Visibility.Visible;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility vsb)
+            {
+                if (vsb == Visibility.Collapsed)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return true;
+        }
+    }
+}
