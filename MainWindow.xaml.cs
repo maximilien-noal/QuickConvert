319ead1f343fcadf5dde1495077fa1bca7e57fb6
Maximilien Noal <noal.maximilien@gmail.com>
2020-12-25 08:43:43 +0100

    Confirm exit while trancoding files
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,5 +9,16 @@
             InitializeComponent();
             Language = System.Windows.Markup.XmlLanguage.GetLanguage(System.Threading.Thread.CurrentThread.CurrentUICulture.Name);
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (DataContext != null && DataContext is MainViewModel vm && vm.IsBusy)
+            {
+                if (MessageBox.Show("Conversion en cours. Fermer quand-même ?", "Conversion en cours", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
     }
 }
